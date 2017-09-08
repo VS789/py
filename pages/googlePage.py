90edf4e96527b3b8f67fb56333f5f20409e89308
Valentin Semikov <valentyn.semikov@markit.com>
2017-09-08 12:30:38 +0300

    More comments

--- pages/googlePage.py
+++ pages/googlePage.py
@@ -5,33 +5,49 @@ from pages.ortnecPage import *
 class GooglePage():
 
     driver = webdriver
+
+    #google start search page selectors
     searchField = "#gs_lc0 input:nth-child(1)"
-    resultOfSearch = ".srg"
+
+    #search result selectors
+    listOfSearchResult = ".srg"
     firstResult = ".srg .g:nth-child(1)"
+    linkFromFirstResult = ".srg .g:nth-child(1) .f.kv._SWb ._Rm"
+
+    #first search Result Selectors
+    firstLink = ".srg .g:nth-child(1) .r a"
+    secondLink = ".srg .g:nth-child(1) .s .f.kv._SWb > a"
 
     def __init__(self,driver):
         self.driver = driver
+        #1. Open google.com
         driver.get("http://google.com/")
-
-    #check that page is loaded
-    def googleIsLoaded(self):
+        # check that page is loaded
         self.driver.find_element_by_css_selector(css_selector=self.searchField)
 
-    #get text and put it into search field, after that imitate Enter press on keyboard
+    #input text in search field, after that imitate Enter press on keyboard and wait result of search
     def doSearchByTextUsingEnter(self,text):
         searchField = self.driver.find_element_by_css_selector(self.searchField)
+        #2. Type Ortnec in search input field
         searchField.send_keys(text)
+        #3. Press "Enter" button
         searchField.send_keys(Keys.ENTER)
-        self.driver.find_element_by_css_selector(self.resultOfSearch)
+        #wait result of search
+        self.driver.find_element_by_css_selector(self.listOfSearchResult)
 
+    #return text of first link from search result
+    def getLinkOfFirstResult(self):
+        return self.driver.find_element_by_css_selector(css_selector=self.linkFromFirstResult).text
+
+
+    #4. On Search Result page click on first link
     #use first link(Big blue link) for navigation
     def openFirstResultFirstLink(self):
-        firstLink = self.driver.find_element_by_css_selector(css_selector=".srg .g:nth-child(1) .r a")
-        firstLink.click()
+        self.driver.find_element_by_css_selector(css_selector=self.firstLink).click()
         return OrtnecPage(self.driver)
 
+    #4. On Search Result page click on first link
     #use second link (Navigete to this page) for navigation
     def openFirstResultSecondLink(self):
-        secondLink = self.driver.find_element_by_css_selector(css_selector=".srg .g:nth-child(1) .s .f.kv._SWb > a")
-        secondLink.click()
+        self.driver.find_element_by_css_selector(css_selector=self.secondLink).click()
         return OrtnecPage(self.driver)
--- tests/OrtnectSearchViaGoogleSearch.py
+++ tests/OrtnectSearchViaGoogleSearch.py
@@ -10,16 +10,24 @@ class OrtnectSearchViaGoogleSearch(unittest.TestCase):
 
     #this tets checks that 'ortnec' text is present after navigation via first link (Big blue link) in serch result list
     def test_search_via_first_link_in_search_result(self):
+        #1. Open google.com and check that page is loaded
         page = GooglePage(self.driver)
-        page.googleIsLoaded()
+        #2. Type Ortnec in search input field and #3. Press "Enter" button
         page.doSearchByTextUsingEnter('Ortnec')
+        #a) check http://ortnec.com link is first in the list of results
+        assert "ortnec.com" == page.getLinkOfFirstResult().text
+        #b) for EACH link on the 1-st result page: click on link and check 'ortnec' text is present on opened page
         assert "ortnec" in page.openFirstResultFirstLink().getCopyrightBlockText()
 
     #this tets checks that 'ortnec' text is present after navigation via second link (Navigate to this page) in serch result list
     def test_search_via_second_link_in_google_search_result(self):
+        #1. Open google.com and check that page is loaded
         page = GooglePage(self.driver)
-        page.googleIsLoaded()
+        #2. Type Ortnec in search input field and #3. Press "Enter" button
         page.doSearchByTextUsingEnter('Ortnec')
+        #a) check http://ortnec.com link is first in the list of results
+        assert "ortnec.com" == page.getLinkOfFirstResult().text
+        #b) for EACH link on the 1-st result page: click on link and check 'ortnec' text is present on opened page
         assert "ortnec" in page.openFirstResultSecondLink().getCopyrightBlockText()
 
     def tearDown(self):
